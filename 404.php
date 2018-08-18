a6631525035cbea60cd84ba8fbe357fede36ba2f
Christian Ekrem <cekrem@Christians-MacBook-Pro.local>
2018-08-18 23:54:09 +0200

    tweaks

--- 404.php
+++ 404.php
@@ -13,47 +13,10 @@ get_header();
 
     <section>
         <header>
-            <h1><?php esc_html_e( 'Oops! That page can&rsquo;t be found.',
+            <h1><?php esc_html_e( 'Siden finnes ikke!',
 					'susty' ); ?></h1>
         </header><!-- .page-header -->
 
-        <div>
-            <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?',
-					'susty' ); ?></p>
-
-			<?php
-			get_search_form();
-
-			the_widget( 'WP_Widget_Recent_Posts' );
-			?>
-
-            <div class="widget widget_categories">
-                <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories',
-						'susty' ); ?></h2>
-                <ul>
-					<?php
-					wp_list_categories( [
-						'orderby'    => 'count',
-						'order'      => 'DESC',
-						'show_count' => 1,
-						'title_li'   => '',
-						'number'     => 10,
-					] );
-					?>
-                </ul>
-            </div><!-- .widget -->
-
-			<?php
-			/* translators: %1$s: smiley */
-			$susty_wp_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s',
-					'susty' ), convert_smilies( ':)' ) ) . '</p>';
-			the_widget( 'WP_Widget_Archives', 'dropdown=1',
-				"after_title=</h2>$susty_wp_archive_content" );
-
-			the_widget( 'WP_Widget_Tag_Cloud' );
-			?>
-
-        </div>
     </section>
 
 
