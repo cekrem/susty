27077d247dd079c3d100710229f1825206e92a13
Christian Ekrem <cekrem@Christians-MacBook-Pro.local>
2018-08-23 19:48:18 +0200

    added analytics

--- footer.php
+++ footer.php
@@ -30,5 +30,16 @@
 
 <?php wp_footer(); ?>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-124490797-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-124490797-1');
+</script>
+
+
 </body>
 </html>
